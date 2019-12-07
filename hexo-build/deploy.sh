3fd2bfd1fac01c2020c79f6bbc248824cd1acfaa
lingc <382150914@qq.com>
2019-12-07 17:20:31 +0800

    rm -rf hexo-blog

--- hexo-build/deploy.sh
+++ hexo-build/deploy.sh
@@ -3,6 +3,7 @@
 git config --global user.email "382150914@qq.com"
 git config --global user.name "lingc"
 cd /app
+rm -rf hexo-blog
 git clone https://github.com/linguochi/hexo-blog.git
 cd /app/hexo-blog
 npm install
