3c482c39ac86ed04908d48422f15502c0707613a
lingc <382150914@qq.com>
2019-12-07 17:47:21 +0800

    淘宝镜像npm

--- generate-lingc-image.sh
+++ generate-lingc-image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd ./hexo-build && docker build -t linguochi/hexo-build:latest .
-cd ./hexo-blog && docker build -t linguochi/hexo-blog:latest .
-cd ./webhook && docker build -t linguochi/lingc-webhook:latest .
+cd ./hexo-build && docker build -t linguochi/hexo-build:latest . && cd ../
+cd ./hexo-blog && docker build -t linguochi/hexo-blog:latest . && cd ../
+cd ./webhook && docker build -t linguochi/lingc-webhook:latest . && cd ../
