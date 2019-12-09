98b14f2efa62eaf7bc0a8fdab387f98a11415042
lingc <382150914@qq.com>
2019-12-09 13:56:44 +0800

    淘宝镜像npm

--- webhook/deploy.sh
+++ webhook/deploy.sh
@@ -4,6 +4,7 @@ git config --global user.email "382150914@qq.com"
 git config --global user.name "lingc"
 
 cd /app/hexo-blog
+git stash
 git pull origin master
 
 npm install  --registry https://registry.npm.taobao.org
