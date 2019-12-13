68467671c01e349b9fee29aba55fe2a5cd5fe870
lingc <382150914@qq.com>
2019-12-13 17:52:53 +0800

    使用express 跳转

--- generate-lingc-image.sh
+++ generate-lingc-image.sh
@@ -3,3 +3,4 @@
 cd ./hexo-build && docker build -t linguochi/hexo-build:latest . && cd ../
 cd ./hexo-blog && docker build -t linguochi/hexo-blog:latest . && cd ../
 cd ./webhook && docker build -t linguochi/lingc-webhook:latest . && cd ../
+cd ./redirector && docker build -t linguochi/redirector:latest . && cd ../
