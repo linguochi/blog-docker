b7facc2c811d434d0fd87ec25d4c4ffaa54ecb56
僵硬小香蕉 <linguochi@cmiot.chinamobile.com>
2021-11-04 21:30:25 +0800

    更新

--- redirector/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 80;
-
-app.all('*', function(req, res) {
-  return res.redirect(301, `https://linguochi.com${req.path}`);
-});
-
-app.listen(port, () => console.log(`Redirector ${port}!`));
